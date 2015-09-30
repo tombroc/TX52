2bf61c8d0587755841405d97e03094e721257b59
Thomas Brocail <Thomas@Tomsteack.local>
2015-09-30 13:57:34 +0200

    Adding intruder is working with a movement

--- graphics.py
+++ graphics.py
@@ -4,8 +4,10 @@
 import os
 from tkinter import *
 from math import sqrt
+import time
+import random
 
-class Application:
+class Application():
 	def _create_circle_arc(self, x, y, r, **kwargs):
 		if "start" in kwargs and "end" in kwargs:
 			kwargs["extent"] = kwargs["end"] - kwargs["start"]
@@ -15,46 +17,49 @@ class Application:
 
 	Canvas.create_circle_arc = _create_circle_arc;
 
-	def draw_radar(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, WIDTH_SCALE, CANVAS_C):
+	def draw_radar(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C):
 
 		# Detection zone 
 		
 		# Coordonnées
 
-		CANVAS_C.delete("all");
-		VIRTUAL_SCOPE = REAL_SCOPE * WIDTH_CANVAS / WIDTH_SCALE;
+		CANVAS_C.delete("arcs");
+		VIRTUAL_SCOPE = REAL_SCOPE * WIDTH_CANVAS / HEIGHT_SCALE;
 	
 		x0 = WIDTH_CANVAS / 2;
 		y0 = HEIGHT_CANVAS;
 		r = 0;
 
 		while r < VIRTUAL_SCOPE:
-			CANVAS_C.create_circle_arc(x0, y0, r, style='arc', outline="red", width=1, start=30, end=150);
+			CANVAS_C.create_circle_arc(x0, y0, r, style='arc', outline="red", width=1, start=30, end=150, tags="arcs");
 			r += 10;
 
 		return CANVAS_C;
+	
+	def add_intruder(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C, warning_l, intruder_b):
+		
+		# To improve by using threads
+		intruder_b.config(state=DISABLED);
+		Y = 1;
+		X = random.uniform(0, WIDTH_CANVAS);
+		dX = 0;
+		dY = 1;
+		VIRTUAL_SCOPE = REAL_SCOPE * HEIGHT_CANVAS / HEIGHT_SCALE;
 
-	def move_intruder(self, INTRUDER, WIDTH_CANVAS, VIRTUAL_SCOPE, CANVAS_C, X, Y, warning_l):
+		warning_l.grid_forget();
+		CANVAS_C.delete("point");
 		
-		print (X)
-		if (Y < WIDTH_CANVAS*0.75 - VIRTUAL_SCOPE):
-			# affichage
-			CANVAS_C.coords(INTRUDER, X,Y,X+20,Y+20,X-20,Y+20);
-			Y = Y + 5;
-			CANVAS_C.after(50,self.move_intruder(INTRUDER, WIDTH_CANVAS, VIRTUAL_SCOPE, CANVAS_C, X, Y, warning_l));
-		warning_l.grid(row=6, column=2)	
-	
-	def add_intruder(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, WIDTH_SCALE, CANVAS_C, warning_l):
-		
-		# CANVAS_C.delete("point");
-	
-		Y = 0;
-		X = WIDTH_CANVAS/2
-		VIRTUAL_SCOPE = REAL_SCOPE * WIDTH_CANVAS / WIDTH_SCALE;
-		print(Y)
 		intruder = CANVAS_C.create_polygon(X, Y, X+20, Y+20, X-20, Y+20, tags="point");
 		
-		self.move_intruder(intruder, WIDTH_CANVAS, VIRTUAL_SCOPE, CANVAS_C, X, Y, warning_l);
+		while (Y < HEIGHT_CANVAS - VIRTUAL_SCOPE):
+			Y = Y + dY;
+			dX = random.uniform(-1.5, 2);
+			X = X + dX;
+			CANVAS_C.move(intruder,dX,dY);
+			CANVAS_C.update();
+			time.sleep(0.025);
+		warning_l.grid(row=6, column=2);	
+		intruder_b.config(state=NORMAL);
 
 	def __init__(self):
 
@@ -65,7 +70,7 @@ class Application:
 		REAL_SCOPE = 200;
 		
 		# Width equivalency (in meters)
-		WIDTH_SCALE = 400;
+		HEIGHT_SCALE = 400;
 
 		# Scope at scale
 		VIRTUAL_SCOPE = 0;
@@ -93,13 +98,13 @@ class Application:
 		# Scope scale
 		scale_l = Label(win, text='Scope value :');
 		scale_l.grid(row=3, column=2);
-		scale_s = Scale(win, from_=1, to=REAL_SCOPE*2, orient=HORIZONTAL, length=300, command=lambda CANVAS_C = canvas_c: self.draw_radar(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, WIDTH_SCALE, canvas_c));
+		scale_s = Scale(win, from_=1, to=REAL_SCOPE*2, orient=HORIZONTAL, length=300, command=lambda CANVAS_C = canvas_c: self.draw_radar(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, canvas_c));
 		scale_s.set(REAL_SCOPE);
 
 		scale_s.grid(row=4, column=2);
 
 		# Add "ennemi"
-		intruder_b = Button(win, text='Add intruder', command=lambda CANVAS_C = canvas_c: self.add_intruder(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, WIDTH_SCALE, CANVAS_C, warning_l));
+		intruder_b = Button(win, text='Add intruder', command=lambda CANVAS_C = canvas_c: self.add_intruder(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C, warning_l, intruder_b));
 		intruder_b.grid(row=5, column=2);
 
 		# exit button
