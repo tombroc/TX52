15c3c4e0eccf6e87f3ded2b047fe4b39b4466ebc
Thomas Brocail <Thomas@Tomsteack.local>
2015-10-07 21:51:11 +0200

    Threading in progress

--- graphics.py
+++ graphics.py
@@ -4,9 +4,15 @@
 import os
 from drone import Drone
 from tkinter import *
-from math import sqrt
 import time
 import random
+
+REAL_SCOPE = 200;
+VIRTUAL_SCOPE = 0;
+# Height equivalency (in meters)
+HEIGHT_SCALE = 400;
+HEIGHT_CANVAS = 0;
+WIDTH_CANVAS = 0;
 
 class Application():
 	def _create_circle_arc(self, x, y, r, **kwargs):
@@ -18,73 +24,51 @@ class Application():
 
 	Canvas.create_circle_arc = _create_circle_arc;
 
-	def draw_radar(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C):
+	def draw_radar(self, HEIGHT_SCALE, scale_s):
 
 		# Detection zone 
 
-		CANVAS_C.delete("ennemi_0");
-		CANVAS_C.delete("alt_0");
-		CANVAS_C.delete("arcs");
-		VIRTUAL_SCOPE = REAL_SCOPE * HEIGHT_CANVAS / HEIGHT_SCALE;
-	
+		self.CANVAS_C.delete("ennemi_0");
+		self.CANVAS_C.delete("alt_0");
+		self.CANVAS_C.delete("arcs");
+		
+		global VIRTUAL_SCOPE;
+		VIRTUAL_SCOPE = scale_s.get() * HEIGHT_CANVAS / HEIGHT_SCALE;
 		x0 = WIDTH_CANVAS / 2;
 		y0 = HEIGHT_CANVAS;
 		r = 20;
 
 		while r < VIRTUAL_SCOPE:
-			CANVAS_C.create_circle_arc(x0, y0, r, style='arc', outline="red", width=2, start=0, end=180, tags="arcs");
+			self.CANVAS_C.create_circle_arc(x0, y0, r, style='arc', outline="red", width=2, start=0, end=180, tags="arcs");
 			r += 10;
+		code = 1;
+		return code;
 
-		return CANVAS_C;
-	
-	def add_drone(self, REAL_SCOPE, WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C, warning_l, intruder_b, scale_s, KIND, IDENTIFIER):
+	def add_drone(self, HEIGHT_SCALE, warning_l, intruder_b, scale_s, KIND, IDENTIFIER):
 		
 		# To improve by using threads
 		intruder_b.config(state=DISABLED);
 		scale_s.config(state=DISABLED);
-		Z = 20;
-		Y = 1;
-		X = random.uniform(0, WIDTH_CANVAS);
-		dX = 0;
-		dY = 1;
-
-
-		warning_l.grid_forget();
-		CANVAS_C.delete("ennemi_0");
-		CANVAS_C.delete("alt_0");
-		
-		intruder = Drone(KIND, IDENTIFIER, CANVAS_C, X, Y, Z);
+		if KIND == "ennemi":
+			Z = 20;
+			Y = 1;
+			X = random.uniform(0, WIDTH_CANVAS);
+			dX = 0;
+			dY = 1;
+			warning_l.grid_forget();
 		
 		VIRTUAL_SCOPE = REAL_SCOPE * HEIGHT_CANVAS / HEIGHT_SCALE;
+		thread_intruder = Drone(KIND, IDENTIFIER, self.CANVAS_C, X, Y, Z);
 		
 		# For the simulation : the ennemi can approach until we decide it's enough
+		thread_intruder.start();
 
-		while (
-				sqrt(
-						(X - (WIDTH_CANVAS/2)) * (X - (WIDTH_CANVAS/2)) 
-					  + (Y - (HEIGHT_CANVAS)) * (Y - (HEIGHT_CANVAS))
-					) > VIRTUAL_SCOPE):
 
-			if X <= WIDTH_CANVAS/2:
-				a = -1;
-				b = 2;
-				dX = random.uniform(a, b);
-			elif X >= WIDTH_CANVAS/2:
-				a = -2;
-				b = 1;
-				dX = random.uniform(a, b);
-
-			if Z < 3:
-				dZ = 2;
-			else:
-				dZ = random.randint(-1,1);
-
-			X, Y, Z = intruder.move(X, Y, Z, dX, dY, dZ, CANVAS_C, intruder);
-
-			time.sleep(0.05);
 		warning_l.grid(row=9, column=2,columnspan=2);
 		scale_s.config(state=NORMAL);	
 		intruder_b.config(state=NORMAL);
+		code = 1;
+		return code;
 
 	def __init__(self):
 
@@ -92,13 +76,13 @@ class Application():
 		# GLOBAL VARIABLES
 		#def main(self):
 		# CAPTOR SCALE
-		REAL_SCOPE = 200;
+		global REAL_SCOPE;
 		
 		# Height equivalency (in meters)
-		HEIGHT_SCALE = 400;
+		global HEIGHT_SCALE;
 
 		# Scope at scale
-		VIRTUAL_SCOPE = 0;
+		global VIRTUAL_SCOPE;
 
 		# Kind of a drone
 		KIND_ENNEMI = "ennemi";
@@ -121,16 +105,20 @@ class Application():
 		main_l = Label(win, text='Drones interception simulation', font=("Purisa",12,"bold","italic"));
 		main_l.grid(row=1, column=1, padx=10, pady=10);
 		
+		global HEIGHT_CANVAS;
+		global WIDTH_CANVAS;
+		
 		HEIGHT_CANVAS = HEIGHT*0.8;
 		WIDTH_CANVAS = WIDTH*0.66;
-		
-		canvas_c = Canvas(win, width = WIDTH_CANVAS, height = HEIGHT_CANVAS, bg ='#30A030', bd=5, relief=SUNKEN);
-		canvas_c.grid(row=3, column=1, rowspan=20, padx=10, pady=10);
 
+		self.CANVAS_C = Canvas(win, width = WIDTH_CANVAS, height = HEIGHT_CANVAS, bg ='#30A030', bd=5, relief=SUNKEN);
+		self.CANVAS_C.grid(row=3, column=1, rowspan=20, padx=10, pady=10);
+
+		retour = 0;
 		# Scope scale
 		scale_l = Label(win, text='Scope value :');
 		scale_l.grid(row=3, column=2, columnspan=2);
-		scale_s = Scale(win, from_=1, to=REAL_SCOPE*2, orient=HORIZONTAL, length=300, command=lambda CANVAS_C = canvas_c: self.draw_radar(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, canvas_c));
+		scale_s = Scale(win, from_=1, to=REAL_SCOPE*2, orient=HORIZONTAL, length=300, command=lambda code=retour: self.draw_radar(HEIGHT_SCALE, scale_s));
 		scale_s.set(REAL_SCOPE);
 		scale_s.grid(row=4, column=2, columnspan=2);
 
@@ -147,7 +135,7 @@ class Application():
 			else:
 				name.grid(row=8, column=num+1-4);
 		# Add "ennemi"
-		intruder_b = Button(win, text='Add intruder', command=lambda CANVAS_C = canvas_c: self.add_drone(scale_s.get(), WIDTH_CANVAS, HEIGHT_CANVAS, HEIGHT_SCALE, CANVAS_C, warning_l, intruder_b, scale_s, KIND_ENNEMI, ENNEMI_ID));
+		intruder_b = Button(win, text='Add intruder', command=lambda code=retour: self.add_drone(HEIGHT_SCALE, warning_l, intruder_b, scale_s, KIND_ENNEMI, ENNEMI_ID));
 		intruder_b.grid(row=5, column=2, columnspan=2);
 
 		# exit button
