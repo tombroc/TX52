2134fbb8efe92b376a37cc77d2420a7fc1fa1438
Thomas Brocail <Thomas@Tomsteack.local>
2015-10-18 19:59:43 +0200

    Moving some commands from "graphics" to "main" => "graphics" is now only for tkinter window

--- graphics.py
+++ graphics.py
@@ -15,8 +15,28 @@ HEIGHT_SCALE = 400;
 HEIGHT_CANVAS = 0;
 WIDTH_CANVAS = 0;
 NUMBER_ALLY_DRONE = 1;
+CONTINUE = True;
 
 class Application():
+
+	def get_height_canvas(self):
+		return HEIGHT_CANVAS;
+
+	def get_width_canvas(self):
+		return WIDTH_CANVAS;
+
+	def get_ennemi_list(self):
+		return self.ennemi_list;
+
+	def get_CANVAS_C(self):
+		return self.CANVAS_C;
+
+	def get_label_list(self):
+		return self.label_list;
+
+	def get_window(self):
+		return self.win;
+
 	def _create_circle_arc(self, x, y, r, **kwargs):
 		if "start" in kwargs and "end" in kwargs:
 			kwargs["extent"] = kwargs["end"] - kwargs["start"]
@@ -40,6 +60,7 @@ class Application():
 		self.CANVAS_C.delete("arcs");
 
 		VIRTUAL_SCOPE = self.scale_s.get() * HEIGHT_CANVAS / HEIGHT_SCALE;
+
 		x0 = WIDTH_CANVAS / 2;
 		y0 = HEIGHT_CANVAS;
 		r = 20;
@@ -55,19 +76,16 @@ class Application():
 		global HEIGHT_CANVAS;
 		global WIDTH_CANVAS;
 		global HEIGHT_SCALE;
-
-		self.radar.start();
+		print("100")
 
 		self.intruder_b.config(state=DISABLED);
 		self.scale_s.config(state=DISABLED);
 		
 		X = random.uniform(0, WIDTH_CANVAS);
-		Y = 1;
+		Y = 0;
 		Z = 200;
 		self.warning_l.grid_forget();
 
-		VIRTUAL_SCOPE = REAL_SCOPE * HEIGHT_CANVAS / HEIGHT_SCALE;
-		
 		thread_drone = Drone(KIND, IDENTIFIER, self.CANVAS_C, X, Y, Z, self.ennemi_list, []);
 		
 		self.ennemi_list.append(thread_drone);
@@ -83,6 +101,11 @@ class Application():
 
 	def get_app(self):
 		return self;
+	
+	def exit(self):
+		global CONTINUE;
+		CONTINUE = False;
+		quit();
 
 	def __init__(self):
 
@@ -97,11 +120,11 @@ class Application():
 
 		global HEIGHT_CANVAS;
 		global WIDTH_CANVAS;
+		global CONTINUE;
 
-		self.drone_list = [];
 		self.ennemi_list = [];
 
-		label_list = [];
+		self.label_list = [];
 		# Kind of a drone
 		KIND_ENNEMI = "ennemi";
 		KIND_ALLY = "ally";
@@ -118,15 +141,17 @@ class Application():
 
 		# Main window
 		self.win = Tk();
-		self.win.title("Drones interseption");
-		self.win.attributes('-fullscreen', True);		
+		self.win.title("Drone interception");
 		
 		# Simulation zone
-		HEIGHT = self.win.winfo_height();
-		WIDTH = self.win.winfo_width();
+
+		HEIGHT = 1000;
+		WIDTH = 1400
 		
-		HEIGHT_CANVAS = HEIGHT*0.8;
-		WIDTH_CANVAS = WIDTH*0.66;
+		self.win.geometry("%dx%d%+d%+d" % (WIDTH,HEIGHT,(self.win.winfo_screenwidth()-WIDTH)/2,(self.win.winfo_screenheight()-HEIGHT)/2));
+		
+		HEIGHT_CANVAS = 800;
+		WIDTH_CANVAS = 990;
 
 		self.warning_l = Label(self.win, text='/!\\Intruder in the zone/!\\', font=("Purisa",12,"bold","italic"), bg='red');
 		
@@ -150,7 +175,7 @@ class Application():
 			num += 1;
 			name = "DRONE "+str(num)
 			name = Label(self.win, text=name, relief=RAISED, width=10, height=5, bg="green");
-			label_list.append(name);
+			self.label_list.append(name);
 			if num < 3:
 				name.grid(row=6, column=num+1);
 			elif num < 5:
@@ -163,7 +188,8 @@ class Application():
 		self.intruder_b.grid(row=5, column=2, columnspan=2);
 
 		# exit button
-		self.quit_b = Button(self.win,text='Quit', command=quit);
+		self.quit_b = Button(self.win,text='Exit', command=lambda:self.exit());
+
 		self.quit_b.grid(row=20, column=2, padx=10, pady=10);
 		
 
@@ -171,6 +197,3 @@ class Application():
 		#---------------------------------------------------------------------------------------------------------------#
 		#---------------------------------------------------------------------------------------------------------------#
 
-
-		self.radar = Radar(self.drone_list, self.ennemi_list, self.CANVAS_C, label_list);
-		self.win.mainloop();
--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 from graphics import Application
+from radar import Radar
+from drone import Drone
 
 if __name__ == '__main__':
+
+	REAL_SCOPE         = 350;
+	VIRTUAL_SCOPE      = 0;
+	# Height equivalency (in meters)
+	HEIGHT_SCALE       = 400;
+	NUMBER_ALLY_DRONE  = 1;
+	
+	# Possible state of a drone
+	drone_out          = 0;	# Drone on his spot but damage so a fly is impossible 
+	drone_backprogress = 1;	# Drone is coming back of his mission
+	drone_ready        = 2;	# Drone on his spot and operational
+	drone_flying       = 3;	# Drone in mission flying through the target
+	drone_back         = 4;	# Drone back of a mission ready for inspection
+	drone_destroyed    = 5;	# Drone destroyed during a mission
+	drone_detected     = 6;	# Drone ennemi dedetected
+	
+	drone_list = [];
+
 	App = Application();
+	
+	
+	
+	print ("---- Drones 1 to 6 initialization ----");
+	for i in range(6):
+		print ("Drone : "+ str(i+1));
+		X = App.get_width_canvas()/2;
+		Y = App.get_height_canvas()+10;
+		Z = 0;
+		drone = Drone("ally", i, App.get_CANVAS_C(), X, Y, Z, App.get_ennemi_list(), App.get_label_list()[i]);
+		drone_list.append(drone);
+
+
+	Radar = Radar(drone_list, App.get_ennemi_list(), App.get_CANVAS_C(), App.get_label_list());
+	Radar.start();
+
+	App.win.mainloop();
